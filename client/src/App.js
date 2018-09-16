b6a5b555ffe92a9019ecda44b09d81b6e2195d2a
Gav Hanna <gavhanna@gmail.com>
2018-09-16 14:04:44 +0100

    auth actions and register reducer set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Map from "./components/map/Map";
@@ -11,14 +14,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Map} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Map} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
