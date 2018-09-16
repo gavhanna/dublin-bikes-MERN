54cb519275d4f7068821f539096fd15d58a68c11
Gav Hanna <gavhanna@gmail.com>
2018-09-16 11:31:56 +0100

    basic set up with bootstrap

---
+++ client/src/components/map/LocationInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class LocationInfo extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+export default LocationInfo;
