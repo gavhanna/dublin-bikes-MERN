d0f37c5755c511b163a54f2b36315e7823d93ee9
Gav Hanna <gavhanna@gmail.com>
2018-09-18 10:25:01 +0000

    added ternary op to store for redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,9 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    : compose,
   )
 );
 
