a77a3346c083d8cb1fa9de7134910d1058dfe6f7
Gav Hanna <gavhanna@gmail.com>
2018-09-19 21:59:08 +0100

    changed order of routes

--- server.js
+++ server.js
@@ -22,15 +22,7 @@ mongoose
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-    // set static folder
-    app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-}
 
 // Passport
 app.use(passport.initialize());
@@ -40,6 +32,16 @@ require("./config/passport")(passport);
 // User Routes
 app.use("/api/users", users);
 app.use("/api/favourites", favourites);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
