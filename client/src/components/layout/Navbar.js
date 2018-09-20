cef52c0a46db0988ed82a54239e4820d06c2cf40
Gav Hanna <gavhanna@gmail.com>
2018-09-20 13:44:44 +0000

    added data-toggle to nav links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <Link className="nav-link" to="/favourites">Favourites</Link>
+          <Link data-toggle="collapse" className="nav-link" to="/favourites">Favourites</Link>
         </li>
         <li className="nav-item">
           <a
@@ -31,10 +31,10 @@ class Navbar extends Component {
     const guestLinks = (
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
+          <Link data-toggle="collapse" className="nav-link" to="/login">Login</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/register">Register</Link>
+          <Link data-toggle="collapse" className="nav-link" to="/register">Register</Link>
         </li>
       </ul>
     );
